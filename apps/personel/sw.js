3c1f957aa247d68f557428f206f08bfa5dc20062
uzeyirsogukcesme <uzeyirsogukcesme@gmail.com>
2026-08-11 15:12:47 +0300

    PRIME

--- apps/personel/sw.js
+++ apps/personel/sw.js
@@ -1,7 +1,7 @@
 /* Tahmisçi Personel PWA — scope: /personel/ */
 self.TAHMISCI_PWA_CONFIG = Object.freeze({
   appId: "personel",
-  version: "2026.08.11.1",
+  version: "2026.08.11.2",
   scopePath: "/personel/",
   offlineUrl: "/personel/offline.html",
   offlineAssets: [
